504878a9a37dcbfad7b0653a6e8b644a2e03d65d
MakaronniyMonstr <57591579+MakaronniyMonstr@users.noreply.github.com>
2021-10-13 19:08:59 +0300

    +  JSON schemas matcher +  GET parameter tests -? posts/comments -  tests for range parameters

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,84 +1,168 @@
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
-import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import pojo.PojoModel;
+import pojo.PostDTO;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 import static io.restassured.RestAssured.*;
+import static io.restassured.module.jsv.JsonSchemaValidator.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 public class FirstTest {
     private static RequestSpecification spec;
-    private static Gson gson;
-    private PojoModel[] overallElements;
 
     @BeforeAll
     static void init_spec() {
         spec = new RequestSpecBuilder()
                 .setContentType(ContentType.JSON)
                 .setBaseUri("https://jsonplaceholder.typicode.com")
+                //.addFilter(new RequestLoggingFilter())
+                .addFilter(new ResponseLoggingFilter())
                 .build();
-
-        gson = new GsonBuilder().create();
     }
 
     @Test
-    public void get_overall_elements() {
-        Response response = given(spec)
-                .when()
-                .get("posts");
-
-        assertThat(response.statusCode(), is(200));
-        overallElements = gson.fromJson(response.body().asString(), PojoModel[].class);
-        assertThat(overallElements.length, greaterThan(0));
+    public void getAllElements() {
+        given(spec).
+                when().
+                    get("posts").
+                then().
+                    statusCode(is(200)).
+                    body(matchesJsonSchemaInClasspath("pojo_list_schema.json")).
+                    body("$.size", greaterThan(0));
     }
 
     @Test
-    public void get_limited_resources() {
-        Response response = given(spec)
-                .param("limit", 1)
-                .when()
-                .get("posts");
+    public void getElementByValidId() {
+        int id = 1;
 
-        assertThat(response.statusCode(), is(200));
-        PojoModel[] pojoModels = gson.fromJson(response.body().asString(), PojoModel[].class);
-        assertThat(pojoModels.length, lessThanOrEqualTo(20));
+        given(spec).
+                when().
+                    get("posts/" + id).
+                then().
+                    statusCode(is(200)).
+                    body(matchesJsonSchemaInClasspath("pojo_schema.json")).
+                    body("id", equalTo(id));
     }
 
     @Test
-    public void get_resource_by_valid_user_id() {
-        Response response = given(spec)
-                .param("userId", 1)
-                .when()
-                .get("posts");
+    public void getElementByInvalidId() {
+        int id = 101;
 
-        assertThat(response.getStatusCode(), is(200));
-        List<PojoModel> pojoModels = Arrays.asList(gson.fromJson(response.body().asString(), PojoModel[].class));
-        System.out.println(pojoModels.get(0).getUserId());
-        assertThat(pojoModels.stream().allMatch(m -> m.getUserId().equals(1L)), is(true));
+        PostDTO post =
+                given(spec).
+                when().
+                    get("posts/" + id).
+                then().
+                    statusCode(is(404)).
+                    extract().as(PostDTO.class);
+
+        // Is equals to empty element (all fields are null)
+        assertThat(post, equalTo(new PostDTO()));
     }
 
     @Test
-    public void get_resource_by_invalid_user_id() {
-        Response response = given(spec)
-                .param("userId", 100)
-                .when()
-                .get("posts");
+    public void getLimitedResources() {
+        int limit = 20;
 
-        assertThat(response.getStatusCode(), is(200));
+        given(spec).
+                    param("size", limit).
+                when().
+                    get("posts").
+                then().
+                    statusCode(is(200)).
+                    body("$.size", lessThanOrEqualTo(limit));
+    }
 
-        List<PojoModel> pojoModels = gson.fromJson(response.body().asString(), ArrayList.class);
-        assertThat(pojoModels.size(), is(0));
+    @Test
+    public void filterResourcesByValidUserId() {
+        int userId = 1;
+
+        PostDTO[] posts =
+                given(spec).
+                        param("userId", userId).
+                    when().
+                        get("posts").
+                    then().
+                        statusCode(is(200)).
+                        extract().as(PostDTO[].class);
+
+        assertThat(posts.length, greaterThan(0));
+        // All requested items contain specified userId
+        assertThat(
+                Arrays.stream(posts).
+                        allMatch(p -> p.getUserId().equals(userId)),
+                is(true));
+    }
+
+    @Test
+    public void filterResourcesByInvalidUserId() {
+        long userId = 100;
+
+        PostDTO[] posts =
+                given(spec).
+                        param("userId", userId).
+                    when().
+                        get("posts").
+                    then().
+                        statusCode(is(200)).
+                        extract().as(PostDTO[].class);
+
+        // There are no elements in response bc userId is invalid
+        assertThat(posts.length, is(0));
+    }
+
+    @Test
+    public void filterByValid2Parameters() {
+        int id = 5;
+        int userId = 1;
+
+        PostDTO[] posts =
+                given(spec).
+                        param("id", id).
+                        param("userId", userId).
+                    when().
+                        get("posts").
+                    then().
+                        statusCode(is(200)).
+                        extract().as(PostDTO[].class);
+
+        assertThat(posts.length, greaterThan(0));
+        assertThat(
+                Arrays.stream(posts)
+                        .allMatch(p -> p.getId().equals(id) &&
+                                        p.getUserId() == userId),
+                is(true));
+    }
+
+    @Test
+    public void filterByValid4Parameters() {
+        int userId = 1;
+        String title = "nesciunt quas odio";
+        String body = "repudiandae veniam quaerat sunt sed\nalias aut fugiat sit autem sed est\nvoluptatem omnis possimus esse voluptatibus quis\nest aut tenetur dolor neque";
+
+        PostDTO[] posts =
+                given(spec).
+                        param("userId", userId).
+                        param("title", title).
+                        param("body", body).
+                    when().
+                        get("posts").
+                    then().
+                        statusCode(200).
+                        extract().as(PostDTO[].class);
+
+        assertThat(posts.length, greaterThan(0));
+        assertThat(
+                Arrays.stream(posts)
+                        .allMatch(p -> p.getUserId() == userId &&
+                                        p.getTitle().equals(title) &&
+                                        p.getBody().equals(body)),
+                is(true));
     }
 }
---
+++ src/test/java/pojo/PostDTO.java
@@ -0,0 +1,11 @@
+package pojo;
+
+import lombok.Data;
+
+@Data
+public class PostDTO {
+    private Integer userId;
+    private Integer id;
+    private String title;
+    private String body;
+}
